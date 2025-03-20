3162f0e452a29ae05b38d35bb56e612c42ee92b0
danielamora13 <danielamora13@hotmail.com>
2025-03-20 16:47:50 +0100

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MaterialModule } from '../material/material.module';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+@NgModule({
+  declarations: [
+    LayoutPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
+  imports: [
+    AuthRoutingModule,
+    CommonModule,
+    MaterialModule
+  ]
+})
+export class AuthModule { }
